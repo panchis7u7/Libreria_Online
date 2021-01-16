97c4650cd832c2755c8f2b77b5a51c51b3672ed6
Panchis7u7 <smadrigal.rod@gmail.com>
2021-01-16 13:52:07 -0600

    Entradas para la conexion de la base de datos creadas y listas.

--- cliente/src/Components/Vistas/main.js
+++ cliente/src/Components/Vistas/main.js
@@ -10,6 +10,22 @@ export default class Main extends React.Component {
         this.state = {
             libros: [],
         }
+        //this.fetchLibros();
+    }
+
+    fetchLibros = () => {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        fetch("http://localhost:3001/cliente", {
+            method: "GET",
+            headers: headers,
+        })
+        .then((respuesta) => respuesta.json())
+        .then((resultado) => {
+            this.setState({
+                libros: resultado.respuesta,
+            });
+        }).catch((error) => console.log("error: ", error));
     }
 
     render(){
@@ -17,16 +33,31 @@ export default class Main extends React.Component {
             <main className="cuerpo">
                 <div className="banner">
                     <Image className="banner-img" src="https://images.wallpaperscraft.com/image/monstera_leaves_art_128418_1920x1080.jpg" fluid></Image>
-{/*                   <Image className="banner-img" src="https://i.pinimg.com/originals/09/2b/8e/092b8e002e1e7b3ddc6fec7cf70b1b88.jpg" fluid></Image> */}
+                 {/*<Image className="banner-img" src="https://i.pinimg.com/originals/09/2b/8e/092b8e002e1e7b3ddc6fec7cf70b1b88.jpg" fluid></Image> */}
                     <p className="centered"><strong>Suscribete</strong></p>
                 </div>
+
+                {/*---------------------------------------------------------------------------------------------------------------- 
+                Recepcion automatica de libros de la BD.
+                En cuanto la base de datos este lista, jalar los libros dinamicamente de la BD.
+
+                <Container className="popular"></Container>
+                    {this.state.libros.map((item) => {
+                        return (
+                            <Book titulo={item.titulo} author={item.autor} precio={item.precio} portada={item.portada} ></Book>
+                        );
+                    })}
+                <Container className="popular"></Container> 
+
+                ----------------------------------------------------------------------------------------------------------------*/}
+                
                 <Container className="popular">
-                    <Book title="C# 7.0 in a nutshell" author="Joseph Albahari" price="0.00" cover="https://books.google.com.mx/books/content?id=HrE5DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72P6NFH_GBPZvGUdOjr2xW3m-csKeB1I0yHZV8-nCa9MMXKqXvdZzfyjtrv71UOmCvaxhnf5JCrkIXG-IgkSWnfqxxGinZ5Kz_uB4h0G6aujK_zvzcm1bsgu7-2QHrbvNoT3olX"></Book>
-                    <Book title="Teach yourself c++" author="Jesse Liberty" price="0.00" cover="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
-                    <Book title="Teach yourself c++" author="Jesse Liberty" price="0.00" cover="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
-                    <Book title="Teach yourself c++" author="Jesse Liberty" price="0.00" cover="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
-                    <Book title="Teach yourself c++" author="Jesse Liberty" price="0.00" cover="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
-                    <Book title="Teach yourself c++" author="Jesse Liberty" price="0.00" cover="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
+                    <Book titulo="C# 7.0 in a nutshell" autor="Joseph Albahari" precio="0.00" portada="https://books.google.com.mx/books/content?id=HrE5DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72P6NFH_GBPZvGUdOjr2xW3m-csKeB1I0yHZV8-nCa9MMXKqXvdZzfyjtrv71UOmCvaxhnf5JCrkIXG-IgkSWnfqxxGinZ5Kz_uB4h0G6aujK_zvzcm1bsgu7-2QHrbvNoT3olX"></Book>
+                    <Book titulo="Teach yourself c++" author="Jesse Liberty" precio="0.00" portada="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
+                    <Book titulo="Teach yourself c++" author="Jesse Liberty" precio="0.00" portada="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
+                    <Book titulo="Teach yourself c++" author="Jesse Liberty" precio="0.00" portada="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
+                    <Book titulo="Teach yourself c++" author="Jesse Liberty" precio="0.00" portada="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
+                    <Book titulo="Teach yourself c++" author="Jesse Liberty" precio="0.00" portada="https://books.google.com.mx/books/content?id=hklfknZ02eMC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE734F10llLTI4kehZ9-lE2Y_uAMixskf6AFnezhhEWm8ItMX1GIF2yeIQ8TQR9LnkalqLTvSUzlLF-hfrwwx-uzlweSi3plk2EBMiHUMxKiPMyZW4VlGhnJ9tBq8UMCgcg4XYmsn"></Book>
                 </Container>
             </main>
         );
@@ -37,10 +68,10 @@ class Book extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            title: props.title,
-            author: props.author,
-            cover: props.cover,
-            price: props.price,
+            titulo: props.titulo,
+            autor: props.autor,
+            portada: props.portada,
+            precio: props.precio,
         }
     }
 
@@ -48,18 +79,18 @@ class Book extends React.Component {
         return (
             <div className="book">
             <div className="book-top">
-                <div className="book-title">{this.state.title}</div>
-                <div className="book-authors">{this.state.author}</div>
+                <div className="book-title">{this.state.titulo}</div>
+                <div className="book-authors">{this.state.autor}</div>
             </div>
             <div className="book-cover"
                 style={{
                     width: 128,
                     height: 193,
                     backgroundImage:
-                    `url('${this.state.cover}')`,
+                    `url('${this.state.portada}')`,
                 }}/>
             <div className="book-footer">
-                <div className="book-price">Precio: ${this.state.price}</div>
+                <div className="book-price">Precio: ${this.state.precio}</div>
             </div>
         </div>
         );
