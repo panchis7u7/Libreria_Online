6ba98d6311620d46e72e4ebda8be5d657d557359
Panchis7u7 <smadrigal.rod@gmail.com>
2021-01-17 14:26:41 -0600

    Prueba.

--- cliente/src/Components/Vistas/main.js
+++ cliente/src/Components/Vistas/main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Container, Image} from 'react-bootstrap';
 import Carousel from 'react-multi-carousel'; 
 import "react-multi-carousel/lib/styles.css";
 import '../../SCSS/libreria.scss';
