b83af4435923aa9c97e4a2172c711924059c6fa7
CarlosVilla42 <cardani_451@hotmail.com>
2021-01-19 17:44:57 -0600

    Update librosController.php

--- servidor/app/Http/Controllers/librosController.php
+++ servidor/app/Http/Controllers/librosController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; 
+use App\Libro
 
 class librosController extends Controller
 {
     public function index() {
         $libros = DB::table('Libros')->get();
         print_r($libros);
+    }
+    public function addLibro(Request $request){
+        
     }
 }
