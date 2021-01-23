e0cccd7afeaaa7c38fb1857a783c695a020f02dc
Panchis7u7 <smadrigal.rod@gmail.com>
2021-01-22 20:34:27 -0600

    Re-estructuracion de archivos scss.

--- cliente/src/Components/Vistas/autores.js
+++ cliente/src/Components/Vistas/autores.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, FormControl, FormLabel, Button, Alert, Row, Table} from 'react-bootstrap';
-import '../../SCSS/libro.scss';
+import '../../SCSS/Base.scss';
 import Popup from 'reactjs-popup';
 
 
