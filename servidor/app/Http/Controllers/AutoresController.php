746a4ead36491901ccba4dd243fa381c87309d1f
Panchis7u7 <smadrigal.rod@gmail.com>
2021-01-23 14:28:30 -0600

    Modelo en laravel para la insercion de autor incluyendo llaves foraneas.

--- servidor/app/Http/Controllers/AutoresController.php
+++ servidor/app/Http/Controllers/AutoresController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Autor;
+use Illuminate\Support\Facades\DB;
 
 class AutoresController extends Controller
 {
@@ -24,7 +26,32 @@ class AutoresController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $id_provincia = DB::table('provincias')->insertGetId(array(
+                'nombre' => $request->input('localidad')
+            ));
+
+            $id_localidad = DB::table('localidades')->insertGetId(array(
+                'nombre' => $request->input('provincia'),
+                'id_provincia' => $request->input($id_provincia)
+            ));
+
+            $id_autor = DB::table('autores')->insertGetId(array('nombre' => $request->input('nombre'),
+            'apellidos' => $request->input('apellidos'),
+            'direccion' => $request->input('direccion'),
+            'email' => $request->input('email'),
+            'telefono' => $request->input('telefono'),
+            'url' => $request->input('url'),
+            'id_localidad' => $request->input($id_localidad)
+            ));
+
+            DB::commit();
+            return response()->json(['id_autor' => $id_autor, 'status' => 'Insercion Exitosa!', 'status_code' => '1']);
+        } catch (\Exception $e){
+            DB::rollback();
+            return response()->json(['id_autor' => '-1', 'status' => 'Insercion Fallida!', 'status_code' => '-1']);
+        }
     }
 
     /**
