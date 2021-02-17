c567ae0683c92f5ec295f70f36f89c62ed275613
Panchis7u7 <smadrigal.rod@gmail.com>
2021-02-17 17:07:43 -0600

    Linkinkg models.

--- servidor/app/Models/Libro.php
+++ servidor/app/Models/Libro.php
@@ -8,8 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Libro extends Model
 {
     use HasFactory;
-
+    
     public $timestamps = false;
+    public $incrementing = true;
+
+    protected $table = 'libros';
+    protected $primaryKey = 'id_libro';
+    protected $keyType = 'int';
 
     protected $fillable = [
         'isbn',
@@ -23,16 +28,4 @@ class Libro extends Model
         'lugar_impresion',
         'url',
     ];
-    /*
-    return [
-        'isbn' => $this->faker->unique()->regexify('[A-Za-z0-9]{15}'),
-        'anio_publicacion' => $this->faker->date('Y-m-d','now'),
-        'descripcion'=> $this->faker->str_random(20),
-        'titulo' => $this->faker->str_random(20),
-        'precio_fisico' => $this->faker->randomFloat(3, 0, 1000),
-        'precio_electronico' => $this->faker->randomFloat(3, 0, 1000),
-        'tamanio' => $this->faker->str_random(4),
-        'fecha_impresion' => $this->faker->date('Y-m-d','now'),
-        'lugar_impresion' => $this->faker->regexify('[A-Za-z0-9]{10}'),
-    ];*/
 }
